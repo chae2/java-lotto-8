b4c1086273ac61bcd343a9f1ced2f8c3ee16e018
chae2 <saintcharry22@gmail.com>
2025-11-03 23:28:03 +0900

    fix: Lotto 예외처리 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,17 @@
 package lotto;
 
+import form.InputFormValidator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplication(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -14,9 +19,24 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validateDuplication(List<Integer> numbers){
         Set<Integer> numberSet = new HashSet<>(numbers);
         if (numberSet.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복이 존재합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        int LOTTO_HIGH = 45;
+        int LOTTO_LOW = 1;
+
+        for (Integer number : numbers) {
+            if (number > LOTTO_HIGH || number < LOTTO_LOW){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
     public List<Integer> getNumbers(){
