a9a0eca86e8f16b89651c54e380b8ecfdedd35eb
Chaeil Moon <102636528+chae2@users.noreply.github.com>
2025-11-03 22:43:38 +0900

    fix: Lotto의 `numbers` 중복값 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복이 존재합니다.");
     }
 
     public List<Integer> getNumbers(){
